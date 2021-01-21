b9a294772221b85ed71e4ebeb6865ac71217d161
Zain Ul Abedin <drafthacker98@gmail.com>
2021-01-21 17:18:42 +0500

    Updates

---
+++ precache-manifest.6fe733b7d89eb5fdf57d155e50312680.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f8bf69b4902297cc2ce540bfa5efd7",
+    "url": "/zainulabedin62/index.html"
+  },
+  {
+    "revision": "e5a6efc491eb5abf7fdb",
+    "url": "/zainulabedin62/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "1fe541d057f8db9aa29f",
+    "url": "/zainulabedin62/static/css/4.19dfc3d0.chunk.css"
+  },
+  {
+    "revision": "f9e3ead1e5580bbd6d5e",
+    "url": "/zainulabedin62/static/css/main.9fa7f8b2.chunk.css"
+  },
+  {
+    "revision": "e999c29359e215d615af",
+    "url": "/zainulabedin62/static/js/2.36511077.chunk.js"
+  },
+  {
+    "revision": "d0d8190170b812713050bd2e6e947e19",
+    "url": "/zainulabedin62/static/js/2.36511077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5a6efc491eb5abf7fdb",
+    "url": "/zainulabedin62/static/js/3.fccd1f1b.chunk.js"
+  },
+  {
+    "revision": "1fe541d057f8db9aa29f",
+    "url": "/zainulabedin62/static/js/4.5eb50d35.chunk.js"
+  },
+  {
+    "revision": "f9e3ead1e5580bbd6d5e",
+    "url": "/zainulabedin62/static/js/main.8fca39dd.chunk.js"
+  },
+  {
+    "revision": "eab5ed04ab1055d62770",
+    "url": "/zainulabedin62/static/js/runtime-main.93302256.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/zainulabedin62/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/zainulabedin62/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "9de5316103234711518d2ae4def8be46",
+    "url": "/zainulabedin62/static/media/cbtcomply.9de53161.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/zainulabedin62/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/zainulabedin62/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/zainulabedin62/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "b087f905318a53fa4f997f26b33ea046",
+    "url": "/zainulabedin62/static/media/frx.b087f905.png"
+  },
+  {
+    "revision": "ccabfa185eaf33da9b1a4ccb360b26ac",
+    "url": "/zainulabedin62/static/media/gol.ccabfa18.jpeg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/zainulabedin62/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "a8e8397b30facf88e59b05677ace1cb4",
+    "url": "/zainulabedin62/static/media/numl-logo.a8e8397b.png"
+  },
+  {
+    "revision": "d312046be07b69d1f0877bbdfb40d1cf",
+    "url": "/zainulabedin62/static/media/pgc.d312046b.png"
+  },
+  {
+    "revision": "245669628973b783883cb601c0782629",
+    "url": "/zainulabedin62/static/media/profileImage.24566962.jpg"
+  },
+  {
+    "revision": "4c13940910295c9876fadc15dbdd411d",
+    "url": "/zainulabedin62/static/media/ps-logo-black.4c139409.jpg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/zainulabedin62/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/zainulabedin62/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/zainulabedin62/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
